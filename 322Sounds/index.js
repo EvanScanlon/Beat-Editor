137a95b8c5d647d070e25f754d91cd9279fa867a
Michael <mickconnors2014@gmail.com>
2021-12-11 20:13:13 +0000

    Added color flashing and streamlined the sound array

--- 322Sounds/index.js
+++ 322Sounds/index.js
@@ -2,40 +2,40 @@
 -Basic functionality for pressing keys->playing sounds
 -Key sounds will be shortened, slightly too long atm
 -*/ 
-let qwerty = "QWERTYUIOPASDFGHJKLZXCVBNM";
+let qwerty = "QWERASDFZXCV";//"QWERTYUIOPASDFGHJKLZXCVBNM";
 let font,fontsize = 32;
+//button struct
 function button(color,letter,xpos,ypos){
     this.color = color;
     this.xpos = xpos;
     this.ypos = ypos;
     this.letter = letter;
+    this.pressed = false
+    this.flashTime = 0;
 }
-let buttons = [];
-//buttons.push(new button([255,0,0],20,20));
-//console.log(buttons[0]);
-//Arrays to hold drum + key sounds
-let drums = [];
-let keys = [];
+let buttons = [];//Array for buttons
+let sounds = []; //Array to hold sounds
 //audio file extension + path for sounds folder
 const ext = ".wav";
 const path = "./sounds";
 
-//preload occurs before setup or draw, fill arrays with sounds
+//preload occurs before setup or draw, fill array with sounds
 function preload(){
     soundFormats('wav');
-    for(let i = 0; i < 4; i++){
-        drums[i] = loadSound(path+'/perc/perc'+i+ext);
-        buttons.push(new button([255,0,0],qwerty[i],20+i*80,20))
-    }
-    for(let i = 0; i < 2; i++){
-        keys[i] = loadSound(path+'/keys/keys'+i+ext);
-        buttons.push(new button([0,255,0],qwerty[i+10],20+i*80,100))
+    for(let i = 0;i < 6;i++){
+        sounds[i] = loadSound(path+"/sound"+i+ext);
+        if(i < 4){
+            buttons.push(new button([255,0,0],qwerty[i],20+(i%4)*80,20))
+        }
+        else if(i < 8){
+            buttons.push(new button([0,255,0],qwerty[i],20+(i%4)*80,100))
+        }
     }
     font = loadFont("./ahronbd.ttf")
 }
 
 function setup() {
-    let cnv = createCanvas(500, 450);
+    let cnv = createCanvas(1000, 500);
     background(200);  
     textFont(font);
     textSize(fontsize);
@@ -45,6 +45,24 @@ function setup() {
 function draw(){
    background(200);
    for(let i = 0;i < buttons.length;i++){
+       //highlights button when hovered over
+       if(mouseX >= buttons[i].xpos && mouseX <= buttons[i].xpos+75 && mouseY >= buttons[i].ypos && mouseY <= buttons[i].ypos+75 && !buttons[i].pressed){
+           buttons[i].color[2] = 255;
+       }
+       //sets highlight back to default unless its looping
+       else if(!buttons[i].pressed){
+           buttons[i].color[2] = 0;
+       }
+       //checks if button should be flashing and highlights if it is
+       if(buttons[i].flashTime > 0){
+           buttons[i].color[2] = 255;
+           buttons[i].flashTime++;
+           //resets flash time after 5 frames
+           if(buttons[i].flashTime == 5){
+               buttons[i].flashTime = 0;
+           }
+       }
+       //draws button at index i
        fill(buttons[i].color);
        rect(buttons[i].xpos,buttons[i].ypos,75,75);
        fill(0)
@@ -54,12 +72,29 @@ function draw(){
 
 
 function keyTyped(){
+    //gets index of key pressed from qwerty sting and plays the sound
     let index = qwerty.indexOf(key.toUpperCase());
-    if(index < 10){
-        drums[index].play();
-    }
-    else if(index < 19){
-        keys[index-10].play();
+    sounds[index].play();
+    buttons[index].flashTime=1;
+}
+
+function mousePressed(){
+    for(let i = 0;i < buttons.length;i++){
+        if(mouseX >= buttons[i].xpos && mouseX <= buttons[i].xpos+75 && mouseY >= buttons[i].ypos && mouseY <= buttons[i].ypos+75){
+            //if a button is pressed already cut the loop
+            if(buttons[i].pressed){
+                buttons[i].color[2] = 0;
+                buttons[i].pressed = false;
+                sounds[i].stop();
+            }
+            //if a button is pressed loop the sound
+            else{
+                buttons[i].color[2] = 255;
+                buttons[i].pressed = true;
+                sounds[i].loop();
+            }
+            
+        }
     }
 }
 
